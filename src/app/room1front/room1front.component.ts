ddec36ea3d19a013cebd18d45dcf4b1c62f3e68b
timwong91 <wong.hsw@gmail.com>
2019-06-16 07:56:38 -0400

    timer service made and timer works

--- src/app/room1front/room1front.component.ts
+++ src/app/room1front/room1front.component.ts
@@ -1,23 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { ItemsService } from "../items.service";
+import { TimerService } from "../timer.service";
 
 @Component({
-  selector: 'room1front',
-  templateUrl: './room1front.component.html',
-  styleUrls: ['./room1front.component.css']
+  selector: "room1front",
+  templateUrl: "./room1front.component.html",
+  styleUrls: ["./room1front.component.css"]
 })
 export class Room1frontComponent implements OnInit {
   items: any;
-  constructor(private itemsService: ItemsService) { }
+
+  constructor(
+    private itemsService: ItemsService,
+    private timerService: TimerService
+  ) {}
 
   ngOnInit() {
-
     this.itemsService.getItems().subscribe(response => {
       this.items = response;
       console.log(this.items);
-    })
-  
- }
+    });
   }
 
+  startTimer(): void {
+    this.timerService.startTimer();
+  }
 
+  stopTimer(): void {
+    this.timerService.stopTimer();
+  }
+}
