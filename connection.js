57262c542db00d9c80614d026bd5c6e94a65dbe2
timwong91 <wong.hsw@gmail.com>
2019-06-16 07:06:47 -0400

    got sound effect links from API

--- connection.js
+++ connection.js
@@ -1,15 +1,14 @@
-"use strict"
+"use strict";
 
 const { Pool } = require("pg");
 
 const credentials = {
   user: "postgres",
-  password: "Market18!",
+  password: "password",
   host: "localhost",
   port: 5432,
   database: "EscapeRoom",
-  ssl: false,
+  ssl: false
 };
-
 
 module.exports = new Pool(credentials);
