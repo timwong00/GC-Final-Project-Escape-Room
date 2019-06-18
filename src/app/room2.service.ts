89fc17c3a11f21a96d22eba832c72ef2078b22f0
Mirjana <kulics.mirjana@gmail.com>
2019-06-18 11:05:05 -0400

    created tutorial service and fixed room services

--- src/app/room2.service.ts
+++ src/app/room2.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Room2Service {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  getItems() {
+    return this.http.get("/room-2-items", { responseType: "json" });
+  }
+
+  getUnlockItems() {
+    return this.http.get("/room-2-unlock-items", { responseType: "json" });
+  }
 }
+
