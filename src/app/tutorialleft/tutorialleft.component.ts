2f8e8eeb735cbd697fba8aca003bebe0be812da2
timwong91 <wong.hsw@gmail.com>
2019-06-19 10:15:15 -0400

    started to separate items between tutorial front left right

--- src/app/tutorialleft/tutorialleft.component.ts
+++ src/app/tutorialleft/tutorialleft.component.ts
@@ -1,17 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { TutorialroomService } from "../tutorialroom.service";
 
-
 @Component({
-  selector: 'tutorialleft',
-  templateUrl: './tutorialleft.component.html',
-  styleUrls: ['./tutorialleft.component.css']
+  selector: "tutorialleft",
+  templateUrl: "./tutorialleft.component.html",
+  styleUrls: ["./tutorialleft.component.css"]
 })
 export class TutorialleftComponent implements OnInit {
-
-  constructor(private tutorialRoomService: TutorialroomService) { }
+  unlockItems: any;
+  constructor(private tutorialRoomService: TutorialroomService) {}
 
   ngOnInit() {
+    this.tutorialRoomService.getTutorialUnlockItems().subscribe(response => {
+      this.unlockItems = response;
+      this.tutorialRoomService.setUnlockItems(response);
+    });
   }
-
 }
--- src/app/tutorialright/tutorialright.component.ts
+++ src/app/tutorialright/tutorialright.component.ts
@@ -1,17 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { TutorialroomService } from "../tutorialroom.service";
-
+import { InventoryService } from "../inventory.service";
 
 @Component({
-  selector: 'tutorialright',
-  templateUrl: './tutorialright.component.html',
-  styleUrls: ['./tutorialright.component.css']
+  selector: "tutorialright",
+  templateUrl: "./tutorialright.component.html",
+  styleUrls: ["./tutorialright.component.css"]
 })
 export class TutorialrightComponent implements OnInit {
-
-  constructor(private tutorialRoomService: TutorialroomService) { }
+  items: any;
+  constructor(private tutorialRoomService: TutorialroomService) {}
 
   ngOnInit() {
+    this.tutorialRoomService.getTutorialItems().subscribe(response => {
+      this.items = response;
+      // this.inventoryService.setItems(response);
+      console.log(this.items);
+    });
   }
-
 }
