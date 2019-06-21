6fc1eed8ee83c52eb2741b2dadc9242eb4997c39
timwong91 <wong.hsw@gmail.com>
2019-06-21 15:49:44 -0400

    installed cors

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const routes = require("./itemsAPI");
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.json());
 app.use(express.static(__dirname + "/dist"));
 app.use("/", routes);
