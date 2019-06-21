389140687ae56de00b62b676255ce80f3f52ea04
Tim Tucker <tmthydt@gmail.com>
2019-06-21 15:34:01 -0400

    room 3 component all views

--- src/app/room3left/room3left.component.ts
+++ src/app/room3left/room3left.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Room3Service } from "../room3.service";
+import { InventoryService } from "../inventory.service";
+import { MatchService } from "../match.service";
+
 
 @Component({
   selector: 'room3left',
@@ -7,10 +10,35 @@ import { Room3Service } from "../room3.service";
   styleUrls: ['./room3left.component.css']
 })
 export class Room3leftComponent implements OnInit {
+  items: any;
+  unlockItems: any;
+  inventoryItems: any[];
+  itemsToMatch: any[] = [];
 
-  constructor(private room3Service: Room3Service) { }
+  constructor(
+    private room3Service: Room3Service,
+    private inventoryService: InventoryService,
+    private matchService: MatchService
+    ) {}
 
   ngOnInit() {
+    this.items = this.room3Service.items;
+
+    this.unlockItems = this.room3Service.uItems;
+    this.itemsToMatch = this.matchService.itemsToMatch;
+  }
+
+  selectItem(inventoryItem) {
+    this.inventoryService.collectItem(inventoryItem);
+  }
+
+  removeItem(index) {
+    this.room3Service.deleteItem(index);
+  }
+
+  matchItems2(itemToUnlock) {
+    this.matchService.setUnlockItemToMatch(itemToUnlock);
+    this.matchService.checkMatch();
   }
 
 }
