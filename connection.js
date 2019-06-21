e210d7419109978b4ec3472758ad64cff825705c
timwong91 <wong.hsw@gmail.com>
2019-06-21 15:24:27 -0400

    added database

--- connection.js
+++ connection.js
@@ -1,14 +1,18 @@
-"use strict";
-
-const { Pool } = require("pg");
-
-const credentials = {
-  user: "postgres",
-  password: "password",
-  host: "localhost",
-  port: 5432,
-  database: "EscapeRoom",
-  ssl: false
+const pg = require("pg");
+const url = require("url");
+try {
+  require("dotenv").config();
+} catch (e) {
+  console.log(e);
+}
+const params = url.parse(process.env.DATABASE_URL);
+const auth = params.auth.split(":");
+const config = {
+  user: auth[0],
+  password: auth[1],
+  host: params.hostname,
+  port: params.port,
+  database: params.pathname.split("/")[1],
+  ssl: params.hostname !== "localhost"
 };
-
-module.exports = new Pool(credentials);
+module.exports = new pg.Pool(config);
