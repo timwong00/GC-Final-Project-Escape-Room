805a3a727b76fbd9bdf7bab535269acb390dca55
Mirjana <kulics.mirjana@gmail.com>
2019-06-21 14:50:34 -0400

    added items to room 2 front left right

--- src/app/room2left/room2left.component.ts
+++ src/app/room2left/room2left.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Room2Service } from "../room2.service";
+import { MatchService } from "../match.service";
+import { InventoryService } from "../inventory.service";
 
 @Component({
   selector: 'room2left',
@@ -7,10 +9,34 @@ import { Room2Service } from "../room2.service";
   styleUrls: ['./room2left.component.css']
 })
 export class Room2leftComponent implements OnInit {
+  items: any;
+  unlockItems: any;
+  inventoryItems: any[];
+  itemsToMatch: any[] = [];
 
-  constructor(private room2Service: Room2Service) { }
+  constructor(
+    private room2Service: Room2Service,
+    private matchService: MatchService,
+    private inventoryService: InventoryService
+    ) { }
 
   ngOnInit() {
+    this.items = this.room2Service.items;
+    this.unlockItems = this.room2Service.uItems;
+    this.itemsToMatch = this.matchService.itemsToMatch;
+  }
+
+  selectItem(inventoryItem) {
+    this.inventoryService.collectItem(inventoryItem);
+  }
+
+  removeItem(index) {
+    this.room2Service.deleteItem(index);
+  }
+
+  matchItems2(itemToUnlock) {
+    this.matchService.setUnlockItemToMatch(itemToUnlock);
+    this.matchService.checkMatch();
   }
 
 }
