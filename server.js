8ec3454883da3a2572c18489e36e15e36409ca56
timwong91 <wong.hsw@gmail.com>
2019-06-21 15:06:26 -0400

    setting up heroku

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const app = express();
 const routes = require("./itemsAPI");
 
 app.use(express.json());
-app.use(express.static(__dirname+"/dist"));
+app.use(express.static(__dirname + "/dist"));
 app.use("/", routes);
 
-let port = 3000;
+let port = process.env.PORT || 5000;
 app.listen(port, _ => console.log(`Server running on port: ${port}`));
