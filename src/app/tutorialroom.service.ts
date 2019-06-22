4e8cd0d09821771bec59722ab3bd4036b9df4607
timwong91 <wong.hsw@gmail.com>
2019-06-21 22:50:43 -0400

    added localhost 5000 to service endpoints

--- src/app/tutorialroom.service.ts
+++ src/app/tutorialroom.service.ts
@@ -13,11 +13,15 @@ export class TutorialroomService {
   constructor(private http: HttpClient) {}
 
   getTutorialItems() {
-    return this.http.get("/tutorial-items", { responseType: "json" });
+    return this.http.get("http://localhost:5000/tutorial-items", {
+      responseType: "json"
+    });
   }
 
   getTutorialUnlockItems() {
-    return this.http.get("/tutorial-unlock-items", { responseType: "json" });
+    return this.http.get("http://localhost:5000/tutorial-unlock-items", {
+      responseType: "json"
+    });
   }
 
   // hasItems() {
