50c83be263d38f20cbff3258ed8ec249b3229d6d
timwong91 <wong.hsw@gmail.com>
2019-06-27 13:55:37 -0400

    fixed some buttons

--- src/app/match.service.ts
+++ src/app/match.service.ts
@@ -151,12 +151,12 @@ export class MatchService {
       this.selected = false;
       // console.log("Items match");
     } else if (this.itemsToMatch[0] !== this.itemsToMatch[1]) {
-      if (this.itemsToMatch.length == 2){
+      if (this.itemsToMatch.length == 2) {
         this.notAMatch = true;
-        console.log("nomatch hint")
+        console.log("nomatch hint");
       } else {
-        console.log("nomatch no hint")
-        return
+        console.log("nomatch no hint");
+        return;
       }
       this.itemsToMatch = [];
       this.selected = false;
